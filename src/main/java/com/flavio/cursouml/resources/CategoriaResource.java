ce2d441733ab257b080d8d47f9ed88d4b7a35a71
Flavio Al <flvavsgit@gmail.com>
2021-03-13 18:30:31 -0300

    Testando a primeira Classe de dominio

--- src/main/java/com/flavio/cursouml/resources/CategoriaResource.java
+++ src/main/java/com/flavio/cursouml/resources/CategoriaResource.java
@@ -1,8 +1,13 @@
 package com.flavio.cursouml.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.flavio.cursouml.domain.Categoria;
 
 @RestController
 @RequestMapping(value="/categorias")
@@ -10,8 +15,16 @@ import org.springframework.web.bind.annotation.RestController;
 public class CategoriaResource {
 	
 	@RequestMapping(method=RequestMethod.GET)
-	private String listar() {
-		return "Meu Segundo Metodo Rest";
+	private List<Categoria> listar() {
+		
+		Categoria cat1  = new Categoria( 1, "Informatica");
+		Categoria cat2 = new Categoria(2, "Escritorio");
+		
+		List<Categoria> catlist = new ArrayList<Categoria>();
+		catlist.add(cat1);
+		catlist.add(cat2);
+		
+		return catlist;
 
 	}
 	
