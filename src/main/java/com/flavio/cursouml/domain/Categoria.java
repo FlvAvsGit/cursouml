a45a28adc06d1408dcfdd0259c462fb0bf5004d2
Flavio Al <flvavsgit@gmail.com>
2021-03-14 01:28:44 -0300

    Banco de Dados H2

--- src/main/java/com/flavio/cursouml/domain/Categoria.java
+++ src/main/java/com/flavio/cursouml/domain/Categoria.java
@@ -1,13 +1,21 @@
 package com.flavio.cursouml.domain;
 
 import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 // O serializable diz que os objetos dessa classe podem ser convertidos para uma
 // sequencia de bytes. e ai, podem ser gravados em arquivos, trafegados em redes
 
+@Entity
 public class Categoria implements Serializable {
 	
 
 	private static final long serialVersionUID = 1L;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	private String nome;
 		
